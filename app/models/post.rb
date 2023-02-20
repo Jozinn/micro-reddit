59e17a497bf8a7fd74a30f2dc5ff77e720971190
jozinn <g.sierpienn@gmail.com>
2023-02-20 12:33:15 +0000

    some work done

---
+++ app/models/post.rb
@@ -0,0 +1,5 @@
+class Post < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 3}
+    validates :content, presence: true, length: {minimum: 10}
+    belongs_to :user
+end
---
+++ db/migrate/20230220095701_post.rb
@@ -0,0 +1,7 @@
+class Post < ActiveRecord::Migration[7.0]
+  def change
+    add_column :posts, :title, :string
+    add_column :posts, :content, :text
+    add_reference :posts, :user, foreign_key: true
+  end
+end
